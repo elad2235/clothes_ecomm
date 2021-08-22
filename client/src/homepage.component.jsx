3002c19aadda5c106b236944d03b9153aeb0d330
elad2235 <34191499+elad2235@users.noreply.github.com>
2021-08-22 15:48:24 +0300

    fix

---
+++ client/src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+
+const HomePage = () => {
+    return <div className='homepage'>
+        <div>
+            hello
+        </div>
+    </div>
+}
+
+
+
+export default HomePage
