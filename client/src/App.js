bf253de62623243fc6935946af41fca75b561f70
elad2235 <34191499+elad2235@users.noreply.github.com>
2021-08-23 13:42:59 +0300

    collection preview added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
