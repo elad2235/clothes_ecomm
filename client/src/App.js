254f168ce669b0f0206b10c524f92817bbf8ed9a
elad2235 <34191499+elad2235@users.noreply.github.com>
2021-08-23 13:57:11 +0300

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
